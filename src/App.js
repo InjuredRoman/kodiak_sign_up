1485e2a97dada17e4ad5bbde255a88f4bd1e67c6
Roman Kaufman <roman.a.kaufman@gmail.com>
2019-03-04 18:07:47 -0500

    initial stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Form, Field } from 'react-final-form';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name : ""
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.MyForm = this.MyForm.bind(this);
+  }
+  validate() {
+    return true;
+  }
+
+  onSubmit(event) {
+    return;
+  }
+
+  handleSubmit(event) {
+    console.log(event);
+    return;
+  }
+  MyForm() {
+    return <Form
+      onSubmit={this.onSubmit}
+      validate={this.validate}
+      render={({ handleSubmit, pristine, invalid }) => (
+        <form onSubmit={this.handleSubmit}>
+          <h2>Simple Default Input</h2>
+          <div>
+            <label>First Name</label>
+            <Field name="firstName" component="input" placeholder="First Name" />
+          </div>
+
+          <h2>Render Function</h2>
+          <Field
+            name="bio"
+            render={({ input, meta }) => (
+              <div>
+                <label>Bio</label>
+                <textarea {...input} />
+                {meta.touched && meta.error && <span>{meta.error}</span>}
+              </div>
+            )}
+          />
+
+          <h2>Render Function as Children</h2>
+          <Field name="phone">
+            {({ input, meta }) => (
+              <div>
+                <label>Phone</label>
+                <input type="text" {...input} placeholder="Phone" />
+                {meta.touched && meta.error && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+
+          <button type="submit" disabled={pristine || invalid}>
+            Submit
+          </button>
+        </form>
+      )}
+    />;
+            }
   render() {
+    var f = this.MyForm();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {f}
       </div>
     );
   }
