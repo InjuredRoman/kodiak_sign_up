d1c3c28eecbeb27afa02ca43d2a3e851e504ff3b
Jeremy Xue <jeremyxue@gmail.com>
2019-05-06 16:34:04 -0400

    Charts + dashboard updates

--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -11,8 +11,8 @@ import CardBody from 'components/Card/CardBody.jsx';
 import { withStyles } from '@material-ui/core/styles';
 import ChartistGraph from 'react-chartist';
 
-import { confirmed, pending } from 'dummy_data/charts.jsx';
-import { fetch_all_enrollments } from '../../middleend/fetchers';
+import { byCurrentActivity, confirmed, pending } from 'dummy_data/charts.jsx';
+import { fetch_all_enrollments, fetch_all_activities } from '../../middleend/fetchers';
 
 const styles = theme => ({
     root: {
@@ -21,7 +21,7 @@ const styles = theme => ({
     },
     ctLine: {
         stroke: "blue",
-        /* Control the thikness of your lines */
+        /* Control the thickness of your lines */
         strokeWidth: "5px",
         /* Create a dashed line with a pattern */
         strokeDasharray: "1px 2px",
@@ -71,6 +71,21 @@ class Dashboard extends Component {
                 );
             },
             error => {
+                console.log("hihihiihihihihihihi")
+                this.setState({
+
+                    loaded: false,
+                    placeholder: 'Something went wrong.',
+                });
+            }
+        );
+        fetch_all_activities(
+            response => {
+                this.setState({ activities: response, loaded: true }, () =>
+                    console.log(this.state.activities)
+                );
+            },
+            error => {
                 this.setState({
                     loaded: false,
                     placeholder: 'Something went wrong.',
@@ -100,21 +115,43 @@ class Dashboard extends Component {
     }
     */
 
-    filterConfirmedByDay(L) {
-        if (L == null) {
+    // Returns a dictionary with keys as current activities and values as the number of enrollments per activity 
+    currentActivityEnrollments(enrollments, activities){
+      var result = {}
+      if(enrollments == null || activities == null) {
+            return result;
+        }
+      var today = new Date();
+      var todayString = today.toString();
+      var currentActivities = activities.filter(
+        activity => (Date.parse(todayString) >= Date.parse(activity.start_date) && Date.parse(todayString) <= Date.parse(activity.end_date))
+        );
+      for(var i = 0; i < currentActivities.length; i++){
+        result[currentActivities[i].title] = 0
+      }
+      for(var i = 0; i < enrollments.length; i++){
+        if(enrollments[i].activity in currentActivities){
+          result[enrollments[i].activity.title] = result[enrollments[i].activity.title]++;
+        }
+      }
+      return result
+    }
+
+    filterConfirmedByDay(enrollments) {
+        if (enrollments == null) {
             return [];
         }
         var today = new Date();
         var timeElapsedToday = (today.getHours() * 3600000) + (today.getMinutes() * 60000) + (today.getSeconds() * 1000);
         var todayString = today.toString();
-        var sevenDays = L.map(
+        var sevenDays = enrollments.map(
             enrollment => (Date.parse(todayString) - Date.parse(enrollment.updated_at) <= (604800000 - timeElapsedToday))
         );
-        var confirmed = L.map(
+        var confirmed = enrollments.map(
             enrollment => (Date.parse(enrollment.updated_at) - Date.parse(enrollment.created_at) >= 2000)
         );
         var result = [0, 0, 0, 0, 0, 0, 0];
-        L.map((e, i) => {
+        enrollments.map((e, i) => {
           if(confirmed[i] === true && sevenDays[i] == true) {
             var date = new Date(Date.parse(e.updated_at));
             result[date.getDay()] += 1;
@@ -173,6 +210,7 @@ class Dashboard extends Component {
       return arr;
     }
 
+    // Helper function for displaying the correct day of the week on the graph
     shift(days, today){
       var shifted = days.slice(0);
       for(var i=0; i<today; i++){
@@ -183,16 +221,20 @@ class Dashboard extends Component {
 
     render() {
         var today = new Date();
-        var dayOfWeek = today.getDay()
-        var dateRange = this.shift(["S", "M", "T", "W", "T", "F", "S"], dayOfWeek+1)
-        console.log(this.filterConfirmedByDay(this.state.enrollments))
-        console.log(this.filterPendingByDay(this.state.enrollments))
-        confirmed.data.series = [this.shift(this.filterConfirmedByDay(this.state.enrollments), dayOfWeek+1)]
-        pending.data.series = [this.shift(this.filterPendingByDay(this.state.enrollments), dayOfWeek+1)]
-        console.log(confirmed.data.series);
-        console.log(pending.data.series);
+        var dayOfWeek = today.getDay();
+        var dateRange = this.shift(["S", "M", "T", "W", "T", "F", "S"], dayOfWeek+1);
+        var currActivities = this.currentActivityEnrollments(this.state.enrollments, this.state.activities)
+        console.log(this.filterConfirmedByDay(this.state.enrollments));
+        console.log(this.filterPendingByDay(this.state.enrollments));
+        confirmed.data.series = [this.shift(this.filterConfirmedByDay(this.state.enrollments), dayOfWeek+1)];
+        pending.data.series = [this.shift(this.filterPendingByDay(this.state.enrollments), dayOfWeek+1)];
+        byCurrentActivity.data.series = [Object.values(currActivities)];
         confirmed.data.labels = dateRange
         pending.data.labels = dateRange
+        confirmed.options.high = Math.max(...confirmed.data.series[0]) + 10
+        pending.options.high = Math.max(...pending.data.series[0]) + 10
+        byCurrentActivity.options.high = Math.max(...byCurrentActivity.data.series[0]) + 10
+        byCurrentActivity.data.labels = Object.keys(currActivities);
         const { classes } = this.props;
         return (
             // <>
@@ -223,6 +265,20 @@ class Dashboard extends Component {
                                     type="Line"
                                     options={pending.options}
                                     listener={pending.animation}
+                                />
+                        </Paper>
+                    </GridItem>
+                    <GridItem xs={4}>
+                        <Paper className={classes.paper}>
+                                <Typography variant="h4" gutterBottom>
+                                    Enrollments by Current Activities
+                                </Typography>
+                                <ChartistGraph
+                                    className="ct-chart"
+                                    data={byCurrentActivity.data}
+                                    type="Line"
+                                    options={byCurrentActivity.options}
+                                    listener={byCurrentActivity.animation}
                                 />
                         </Paper>
                     </GridItem>
