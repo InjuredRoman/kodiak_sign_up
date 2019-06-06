36ef93257d7270c18ce3817cbdb0675027fffcc3
Roman Kaufman <roman.a.kaufman@gmail.com>
2019-06-06 16:21:04 -0700

    removing yo, test for git on windows server

--- src/components/Admin/Topbar.js
+++ src/components/Admin/Topbar.js
@@ -115,7 +115,6 @@ function Topbar(props) {
     };
     var bottomNavLinks = (
         <Paper>
-        <h1>yo</h1>
         <BottomNavigation value={value} onChange={handleChange} className={classes.root}>
             {props.routes.map((route, i) => {
                 if (route.base === '/admin') {
